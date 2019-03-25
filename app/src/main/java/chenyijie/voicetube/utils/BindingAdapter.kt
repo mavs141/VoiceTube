56893f0ea3fef5eb20ecbff08817cd4bc93ddafd
jay.chen <mavs141@gmail.com>
2019-03-25 23:22:41 +0800

    update some library

--- app/src/main/java/chenyijie/voicetube/utils/BindingAdapter.kt
+++ app/src/main/java/chenyijie/voicetube/utils/BindingAdapter.kt
@@ -4,15 +4,24 @@ import android.databinding.BindingAdapter
 import android.widget.ImageView
 import chenyijie.voicetube.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.request.RequestOptions
+
+
 
 object BindingAdapter {
     @JvmStatic
     @BindingAdapter("imageUrl")
     fun ImageView.loadImage(url: String) {
+        val options = RequestOptions()
+                .centerCrop()
+                .placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)
+                .priority(Priority.HIGH)
+
         Glide.with(this.context)
                 .load(url)
-                .error(this.context.getDrawable(R.mipmap.ic_launcher))
-                .placeholder(this.context.getDrawable(R.mipmap.ic_launcher))
+                .apply(options)
                 .into(this)
     }
 
